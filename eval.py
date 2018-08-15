6526e5a574d1f198eaafbc371957123e7c9275b1
Christopher Eriksen <eriksenc@oregonstate.edu>
2018-08-15 14:42:51 -0700

    Added eval script

--- eval.py
+++ eval.py
@@ -301,6 +301,23 @@ def validate(val_loader, model, criterion, print_freq):
     return top1.avg
 
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
