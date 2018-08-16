e5aa6291afbb605e6a0912a967d3ba8320201136
Christopher Eriksen <eriksenc@oregonstate.edu>
2018-08-16 16:56:31 -0700

    minor

--- train.py
+++ train.py
@@ -92,15 +92,15 @@ def main():
     freeze_weights = False
 
     weights_load_name = 'example_load.pth'
-    weights_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e90_v1.pth'
-    ckpt_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e90_v1_ckpt.pth'
-    best_ckpt_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e90_v1_best_ckpt.pth'
+    weights_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e10_v1.pth'
+    ckpt_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e10_v1_ckpt.pth'
+    best_ckpt_save_name = 'inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e10_v1_best_ckpt.pth'
 
     load_order = True
     subset_instance_order_file = 'instance_order_0.txt'
     test_instances_file = 'test_instances_0.txt'
 
-    accuracies_file = '/home/scatha/lifelong_object_learning/long_term_learning/accuracies/inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e90_v1.txt'
+    accuracies_file = '/home/scatha/lifelong_object_learning/long_term_learning/accuracies/inceptionresnetv2_cifar100_imagenet_sgd_lr0.01_e10_v1.txt'
     ############################################
 
     ## model
