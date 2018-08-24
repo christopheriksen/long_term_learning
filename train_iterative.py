ffaa3a4b1ca453b6c73f12491e529200296379b6
Christopher Eriksen <eriksenc@oregonstate.edu>
2018-08-23 18:15:31 -0700

    minor

--- train_iterative.py
+++ train_iterative.py
@@ -760,10 +760,9 @@ def train_distillation(train_dataset, coreset, old_output, model, criterion, dis
         for i, (input, target) in enumerate(train_loader):
             target = target.cuda(non_blocking=True)
             output, features = model(input)
-            loss += criterion(output, target)
-            # loss += (batch_criterion(output, target)/num_train_data)
+            loss += (batch_criterion(output, target)/num_train_data).data
             # # loss += (criterion(output, target)/num_train_data)
-            # print (loss)
+            print (loss)
 
         # # compute gradient and do SGD step
         # optimizer.zero_grad()
